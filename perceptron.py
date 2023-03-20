38fc63a36c68f92c89f122e4d49b18ef48ff3d8d
oier <oieruni22@gmail.com>
2023-03-20 21:55:58 +0100

    [Refactor] Removing unnecessary output prints

--- perceptron.py
+++ perceptron.py
@@ -46,7 +46,6 @@ class PerceptronClassifier:
         self.features = trainingData[0].keys()  # could be useful later
         # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
         # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
-        print(self.weights)
 
         for iteration in range(self.max_iterations):
             print("Starting iteration ", iteration, "...")
